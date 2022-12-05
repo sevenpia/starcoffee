df6b0ebb97d5388b85b4c0ba384697db0dfa7e86
sevenpia <sevenpia@naver.com>
2022-12-05 16:33:48 +0900

    index3

--- js/script3.js
+++ js/script3.js
@@ -1,5 +1,45 @@
+// 검색영역 - 검색버튼
+/* const searchEl = document.querySelector('.search')
+const searchInputEl = searchEl.querySelector('input')
+searchEl.addEventListener('click', function () {
+  searchInputEl.focus()
+})
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused')
+  searchInputEl.setAttribute('placeholder', '통합검색')
+})
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused')
+  searchInputEl.setAttribute('placeholder', '')
+})
+ */
+
+
+//검색창 제어
+// 검색존 설정(.search) : 검색존에 클릭이 일어나면 포커스 실행시키고  ==  포커스되면 돋보기 모양 없어지도록 한다.
+// searchInputEl : 인풋영역에 포커스가 일어나면 placeholde 설정.
+
+const searchEl = document.querySelector('.search')
+const searchInputEl = document.querySelector('.search input')
+
+searchEl.addEventListener('click', function () {
+  searchInputEl.focus();
+})
+
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused')
+  searchInputEl.setAttribute('placeholder', '통합검색')
+})
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused')
+  searchInputEl.setAttribute('placeholder', '')
+})
+
+
+// 메인이미지 순차적으로 나타나기
 // 나타날 요소들을 하나씩 반복해서 처리!
 // 각 요소들을 순서대로(delay) 보여지게 함!
+// gsap.to(요소, 지속시간(초), 옵션)
 
 /* const fadeEls = document.querySelectorAll('.visual .fade-in')
 fadeEls.forEach(function (fadeEl, index) {
@@ -11,7 +51,6 @@ fadeEls.forEach(function (fadeEl, index) {
 
 
 // gsap.to(요소, 지속시간(초), 옵션)
-
 const fadeEls = document.querySelectorAll('.visual .fade-in')
 console.log(fadeEls)
 fadeEls.forEach(function (fadeEl, index) {
@@ -21,11 +60,11 @@ fadeEls.forEach(function (fadeEl, index) {
   })
 })
 
-
-const youEls = document.querySelectorAll('.youtube .fade-in')
-youEls.forEach(function (youEl, index) {
-  gsap.to(youEl, 1, {
+/* const fadeEls = document.querySelectorAll('.visual .fade-in')
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
     delay: (index + 1) * 0.7,
     opacity: 1
   })
-})
+}) */
+
