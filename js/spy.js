89928fa0d5777f3687bd1765c0c451b2bea20223
sevenpia <sevenpia@naver.com>
2022-12-08 10:37:47 +0900

    spy modi

--- js/spy.js
+++ js/spy.js
@@ -1,3 +1,19 @@
+/* gsap  : 각 이미지 순차적으로 나오기 */
+
+const fadeIns = document.querySelectorAll('.fade-in')
+console.log(fadeIns)
+
+/* gsap 라이브러리 연결 */
+// gsap.to(요소, 지속시간(초), 옵션)
+
+fadeIns.forEach(function (fadeIn, index) {
+  gsap.to(fadeIn, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1
+  })
+})
+
+
 /* 
 const spyEls = document.querySelectorAll('section.spy-scroll')
 console.log(spyEls);
@@ -12,7 +28,7 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller())
 }) */
 
-/******************** 섹션별 움직이는 애니메이션 효과 만들기 ***************** */
+/******************** 섹션별 움직이는 애니메이션 효과 만들기  scrollMagic ***************** */
 
 spyEls = document.querySelectorAll('section.scroll-spy')
 spyEls.forEach(function (spyEl) {
@@ -56,9 +72,6 @@ new Swiper(".swiper", {
     nextEl: '.swiper-button-next',
   }
 })
-
-
-
 
 /* new Swiper(".mySwiper", {
   slidesPerView: 3,
