f33897ba5ed0ce9bf84e59020738e633082cca35
sevenpia <sevenpia@naver.com>
2022-12-16 16:07:36 +0900

    spyscroll

--- js/spy.js
+++ js/spy.js
@@ -1,3 +1,23 @@
+// 검색창 제어
+
+const searchEl = document.querySelector('.search')
+const searchInputEl = searchEl.querySelector('input')
+// 검색창 요소를 클릭하면 실행.
+searchEl.addEventListener('click', function () {
+  searchInputEl.focus()
+})
+// 검색창 요소 내부 실제 input 요소에 포커스되면 실행.
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused')
+  searchInputEl.setAttribute('placeholder', '통합검색')
+})
+// 검색창 요소 내부 실제 input 요소에서 포커스가 해제(블러)되면 실행.
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused')
+  searchInputEl.setAttribute('placeholder', '')
+})
+
+
 /* gsap  : 각 이미지 순차적으로 나오기 */
 
 const fadeIns = document.querySelectorAll('.fade-in')
@@ -124,6 +144,11 @@ new Swiper(".awards .swiper", {
   },
 });
  */
+
+const thisYear = document.querySelector('.this-year')
+
+thisYear.textContent = new Date().getFullYear();
+
 
 
 
